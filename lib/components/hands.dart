3e619181b102ddf93c3221840d078ba7bd73224f
i88seven <i88.jelounjem@gmail.com>
2020-09-12 16:35:29 +0900

    Rerender hands

--- lib/components/hands.dart
+++ lib/components/hands.dart
@@ -6,29 +6,43 @@ import 'package:lama/constants/card_state.dart';
 
 class Hands {
   List<int> numbers = [];
+  List<FrontCard> _cardObjects = [];
   final LamaGame game;
 
   Hands(this.game);
 
   void initialize(List<int> numbers) {
-    numbers.sort();
-    numbers.forEach((number) => drawCard(number));
+    this.numbers = numbers;
+    this.numbers.sort();
+    _render();
   }
 
   void drawCard(number) {
-    Position pos = Position(
-      this.numbers.length * FrontCard.cardSize.width / 2,
-      this.game.screenSize.height - FrontCard.cardSize.height,
-    );
-    this.game.add(FrontCard(number, CardState.Hand)
-      ..x = pos.x
-      ..y = pos.y);
     this.numbers.add(number);
     this.numbers.sort();
+    _render();
   }
 
   void discard(FrontCard card) {
-    this.game.markToRemove(card);
     this.numbers.remove(card.number);
+    _render();
+  }
+
+  void _render() {
+    _cardObjects.forEach((cardObject) {
+      this.game.markToRemove(cardObject);
+    });
+    _cardObjects = [];
+    this.numbers.asMap().forEach((index, number) {
+      Position pos = Position(
+        index * FrontCard.cardSize.width / 2,
+        this.game.screenSize.height - FrontCard.cardSize.height,
+      );
+      FrontCard cardObject = FrontCard(number, CardState.Hand);
+      this.game.add(cardObject
+        ..x = pos.x
+        ..y = pos.y);
+      _cardObjects.add(cardObject);
+    });
   }
 }
