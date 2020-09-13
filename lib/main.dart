d454fb99adafb8d928d1aeec4b6d35523b9ca149
i88seven <i88.jelounjem@gmail.com>
2020-09-13 17:16:33 +0900

    Fix orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:lama/lama_game.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //向き指定
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, //縦固定
+  ]);
   await Firebase.initializeApp();
   final game = LamaGame();
 
